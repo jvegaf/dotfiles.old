e8de5557b28e32516c2e0526589fab4561b9a5cb
Jose Vega <josevega234@gmail.com>
2021-05-04 20:39:45 +0200

    update aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -31,7 +31,7 @@ alias mp='mousepad '
 alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
-alias o.='(nemo $PWD &>/dev/null &)'
+alias o.='(caja $PWD &>/dev/null &)'
 alias up='dot package update_all'
 
 alias ddc='ddcutil setvcp 0x10 '
