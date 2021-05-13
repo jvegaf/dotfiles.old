2a7cfdfe72bd6344508975e9546509e928bd3264
Jose Vega <josevega234@gmail.com>
2021-05-13 11:03:06 +0200

    arch packages install

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -11,6 +11,7 @@ alias cdc="cd ~/Code"
 # Git
 alias gaa="git add -A"
 alias gb="git branch --all"
+alias gbd="git branch -d"
 alias gc="git commit"
 alias gcm="git commit -m"
 alias gca="git add --all && git commit --amend --no-edit"
@@ -26,12 +27,12 @@ alias gs="git status -sb"
 alias gsw="git switch"
 
 # Utils
-alias idea='intellij-idea-ultimate'
+alias idea='intellij-idea-ultimate-edition'
 alias mp='mousepad '
 alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
-alias o.='(nemo $PWD &>/dev/null &)'
+alias o.='(thunar $PWD &>/dev/null &)'
 alias up='dot package update_all'
 
 alias ddc='ddcutil setvcp 0x10 '
