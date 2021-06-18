6064d8dbcc124168ec94f69aab7008a8de03e996
Jose Vega <josevega234@gmail.com>
2021-06-19 01:27:28 +0200

    fix installer

---
+++ os/linux/packages/xBuntu/pop_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install gnome-sushi snapd gnome-tweaks -y
+
+sudo snap install --classic code 
+sudo snap install --classic intellij-idea-community
+sudo snap install --classic gitkraken 
