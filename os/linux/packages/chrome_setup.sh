7da0591ff7fb45885e8c5a602c6b38037a1974f5
Jose Vega <josevega234@gmail.com>
2021-09-06 02:22:26 +0200

    Update chrome_setup.sh

--- os/linux/packages/chrome_setup.sh
+++ os/linux/packages/chrome_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chrome
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 rm -rf google-chrome-stable_current_amd64.deb
