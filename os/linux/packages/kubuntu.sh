c708afea5707285f54a96bc77d032bf4c65a35d3
Jose Vega <josevega234@gmail.com>
2021-09-21 11:05:53 +0200

    auto updated

--- os/linux/packages/kubuntu.sh
+++ os/linux/packages/kubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install -y usb-creator-kde kubuntu-restricted-extras
+
 
 sudo snap install code --classic
-sudo snap install intellij-idea-ultimate --classic
+sudo snap install intellij-idea-community --classic
 
