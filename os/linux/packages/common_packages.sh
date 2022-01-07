6edb6ee106febde4dcd60a83e0dcb69fd5da94f2
Jose Vega <josevega234@gmail.com>
2022-01-07 21:00:02 +0100

    hotfix

--- os/linux/packages/common_packages.sh
+++ os/linux/packages/common_packages.sh
@@ -11,20 +11,3 @@ sudo systemctl enable docker
 sudo usermod -aG docker $USER
 sudo usermod -aG dialout $USER
 
-#emoji-menu       https://github.com/jchook/emoji-menu
-wget 'https://bit.ly/emoji-menu'
-chmod +x emoji-menu
-sudo mv emoji-menu /usr/local/bin
-
-#docker-clean
-curl -s https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker-clean |
-sudo tee /usr/local/bin/docker-clean > /dev/null && \
-sudo chmod +x /usr/local/bin/docker-clean
-
-#nodejs
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install -y nodejs yarn
-
