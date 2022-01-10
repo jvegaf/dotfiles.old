b2d469bd1ce35299e77f15ffc56f00e2184ba856
Jose Vega <josevega234@gmail.com>
2022-01-10 11:20:24 +0000

    Update common_packages.sh

--- os/linux/packages/common_packages.sh
+++ os/linux/packages/common_packages.sh
@@ -6,6 +6,12 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt-get -y install tldr python3-pip vlc build-essential htop screenfetch wget docker.io docker-compose vim simplescreenrecorder rofi xclip xdotool grep coreutils locate
 
+wget -q -O - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add -
+echo "deb [arch=amd64] https://apt.bell-sw.com/ stable main" | sudo tee /etc/apt/sources.list.d/bellsoft.list
+
+sudo apt-get update
+sudo apt-get install -y bellsoft-java17 gradle
+
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
