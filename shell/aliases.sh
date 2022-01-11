1f1ccf091ffabc5a4bbec8eadcaba76862deede4
Jose Vega <josevega234@gmail.com>
2022-01-11 13:25:12 +0100

    upd
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,5 @@
-# Enable aliases to be sudo’ed
-alias sudo='sudo '
+#!/bin/sh
+alias sudo='sudo !!'
 
 alias ..="cd .."
 alias ...="cd ../.."
@@ -7,8 +7,8 @@ alias l="exa -l"
 alias la="exa -la"
 alias ~="cd ~"
 alias cdc="cd ~/Code"
-alias cdf="code $DOTFILES_PATH"
-alias dotfiles="cd $DOTFILES_PATH"
+alias cdf='code $DOTFILES_PATH'
+alias dotfiles='cd $DOTFILES_PATH'
 alias v="bat"
 
 # Git
@@ -20,12 +20,12 @@ alias gcm="git commit -m"
 alias gca="git add --all && git commit --amend --no-edit"
 alias gco="git checkout"
 alias gcl="git clone"
-alias gd="$DOTLY_PATH/bin/dot git pretty-diff"
+alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
 alias gf="git fetch --all -p"
 alias gps="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
-alias gl="$DOTLY_PATH/bin/dot git pretty-log"
+alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 alias grh="git reset --hard"
 alias gs="git status -sb"
 alias gsw="git switch"
@@ -35,9 +35,9 @@ alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='(dolphin $PWD &>/dev/null &)'
-alias up='dot package update_all'
-alias dfcc='dot setup commmit_changes'
-
+alias up='$DOTLY_PATH/bin/dot package update_all'
+alias cddf='code $DOTFILES_PATH'
+alias dsave='$DOTLY_PATH/bin/dot setup commmit_changes'
 alias ddc='ddcutil setvcp 0x10 '
 alias ddcx='ddcutil setvcp 0x10 0'
 alias ddcv='ddcutil setvcp 0x10 100'
