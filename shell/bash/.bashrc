408aff913e5ca5069e948176fc87912e0bb2bdec
Jose Vega <josevega234@gmail.com>
2022-01-13 16:34:22 +0100

    update

--- shell/bash/.bashrc
+++ shell/bash/.bashrc
@@ -1,4 +1,4 @@
-export DOTFILES_PATH="/home/jose/.dotfiles"
+export DOTFILES_PATH="/home/th3g3ntl3m3n/.dotfiles"
 export DOTLY_PATH="$DOTFILES_PATH/modules/dotly"
 export DOTLY_THEME="codely"
 
--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,9 +1,9 @@
-zimfw() { source /home/samsepi0l/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
-zmodule() { source /home/samsepi0l/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
-fpath=(/home/samsepi0l/.dotfiles/shell/zsh/.zim/modules/git-info/functions ${fpath})
+zimfw() { source /home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
+zmodule() { source /home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
+fpath=(/home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/modules/git-info/functions ${fpath})
 autoload -Uz -- coalesce git-action git-info
-source /home/samsepi0l/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
-source /home/samsepi0l/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
-source /home/samsepi0l/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
-source /home/samsepi0l/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /home/samsepi0l/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
+source /home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
+source /home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
+source /home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /home/th3g3ntl3m3n/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
