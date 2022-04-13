56e157f2885d70177c4e28200a2f0282495ba383
Jose Vega <josevega234@gmail.com>
2022-04-13 20:24:44 +0200

    Update common_packages.sh

--- os/linux/packages/common_packages.sh
+++ os/linux/packages/common_packages.sh
@@ -4,7 +4,7 @@ sudo apt -y install software-properties-common
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt-get -y install tldr python3-pip python-is-python3 htop screenfetch wget vim rofi xclip xdotool grep coreutils locate openjdk-17-jdk
+sudo apt-get -y install tldr python3-pip htop screenfetch wget vim rofi xclip xdotool grep coreutils locate
 
 
 
