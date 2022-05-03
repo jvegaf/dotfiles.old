c5414c33fa99959e0cfde2873b0702e548c3bfb5
Jose Vega <josevega234@gmail.com>
2022-05-03 18:23:11 +0200

    auto updated

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -41,7 +41,7 @@ alias grep='grep --color=auto'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias pc.='(pycharm-community $PWD &>/dev/null &)'
-alias o.='(thunar $PWD &>/dev/null &)'
+alias o.='(nautilus $PWD &>/dev/null &)'
 alias up='dot package update_all'
 alias qtdesigner='(qt6-tools designer $PWD &>/dev/null &)'
 
