69f39471b779ac5eb36457616da0f476b1ace23d
Jose Vega <josevega234@gmail.com>
2022-05-11 20:34:45 +0200

    auto updated

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -16,7 +16,10 @@ alias logout="gnome-session-quit"
 
 # Git
 alias gaa="git add -A"
+alias gb="git branch"
+alias gba="git branch --all"
 alias gc='$DOTLY_PATH/bin/dot git commit'
+alias gcl="git clone"
 alias gca="git add --all && git commit --amend --no-edit"
 alias gcm="git commit -m"
 alias gco="git checkout"
@@ -26,10 +29,8 @@ alias gf="git fetch --all -p"
 alias gps="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
-alias gb="git branch"
 alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 alias gsw="git switch"
-alias gcl="git clone"
 
 # Utils
 alias idea='intellij-idea-community'
@@ -41,9 +42,8 @@ alias grep='grep --color=auto'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias pc.='(pycharm-community $PWD &>/dev/null &)'
-alias o.='(nautilus $PWD &>/dev/null &)'
+alias o.='(thunar $PWD &>/dev/null &)'
 alias up='dot package update_all'
-alias qtdesigner='(qt6-tools designer $PWD &>/dev/null &)'
 
 alias ddc='ddcutil setvcp 0x10 '
 alias ddcx='ddcutil setvcp 0x10 0'
