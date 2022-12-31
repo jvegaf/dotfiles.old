864c59fdf114e3f277487575721a765cae23ab01
Jose Vega <josevega234@gmail.com>
2022-12-31 17:13:16 +0100

    Update common_packages.sh

--- os/linux/packages/xBuntu/common_packages.sh
+++ os/linux/packages/xBuntu/common_packages.sh
@@ -4,7 +4,7 @@ sudo apt -y install software-properties-common
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt-get -y install tldr python3-pip htop screenfetch wget neovim rofi xclip xdotool grep coreutils locate
+sudo apt-get -y install tldr python3-pip htop screenfetch wget luarocks xsel rofi xclip xdotool grep coreutils locate
 
 
 
