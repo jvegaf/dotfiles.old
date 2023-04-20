1ff968dfbd0238416b657fa22041d6c9d16cd1e6
Jose Vega <josevega234@gmail.com>
2023-04-20 20:08:08 +0200

    auto updated

---
+++ os/linux/packages/xBuntu/app-image-cli-tool.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo wget https://github.com/MGSousa/appimage-cli-tool/releases/download/latest/appimage-cli-tool -O /usr/local/bin/appimage-cli-tool; 
+sudo chmod +x /usr/local/bin/appimage-cli-tool
+
+echo "AppImage cli tool installed"
+
+exit 0
