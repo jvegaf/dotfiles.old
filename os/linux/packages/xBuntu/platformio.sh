45fb3914acf18848218694d0f598ecae55fe9e57
Jose Vega <josevega234@gmail.com>
2023-09-08 03:31:54 +0200

    platformio

--- os/linux/packages/xBuntu/platformio.sh
+++ os/linux/packages/xBuntu/platformio.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-python3 -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+set -euo pipefail
+
+sudo apt-get install -y python3.10-venv
+
+curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
+python3 get-platformio.py
 
 sudo mkdir -p /usr/local/bin
 sudo ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
@@ -8,3 +13,9 @@ sudo ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
 sudo ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
 
 sudo usermod -a -G dialout $USER
+
+rm -rf get-platformio.py
+
+echo "Platformio installed"o
+
+exit 0
