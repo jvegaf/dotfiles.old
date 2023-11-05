035f12b59a57fe0f711606302218cb5b2b6949dc
Jose Vega <josevega234@gmail.com>
2023-11-05 14:05:41 +0100

    nvm

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -41,7 +41,13 @@ install_linux_extra() {
 }
 
 install_nvm() {
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+	nvm install --lts
+	nvm use lts
 }
 
 paru_setup() {
