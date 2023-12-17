197e53bc28fa000ade34835a0ecceebd79ed4c26
Jose Vega <jvega@tulotero.com>
2023-12-17 21:56:25 +0100

    auto updated

--- os/linux/packages/xBuntu/fusion360-wine.sh
+++ os/linux/packages/xBuntu/fusion360-wine.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+sudo apt-get -y install yad
+
 mkdir -p "$HOME/.fusion360/bin" && cd "$HOME/.fusion360/bin" && wget -N https://raw.githubusercontent.com/alextrical/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/install.sh && chmod +x install.sh && ./install.sh
 
 
