33fc9d0104d19480fd5cc78a468a6af892dc8129
Jose Vega <jvega@tulotero.com>
2023-12-19 00:06:18 +0100

    auto updated

--- os/linux/packages/xBuntu/astronvim-setup.sh
+++ os/linux/packages/xBuntu/astronvim-setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 NVIM_DIR=$HOME/.config/nvim
 NVIM_USER=$NVIM_DIR/lua/user
 
-if [-d "$NVIM_DIR" ]; then rm -Rf $NVIM_DIR; fi
+if [ -d "$NVIM_DIR" ]; then rm -Rf $NVIM_DIR; fi
 
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim $NVIM_DIR
 git clone https://github.com/jvegaf/astronvim_user $NVIM_USER
