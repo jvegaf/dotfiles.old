f0f6d6addc7481776ddf4ec593cbb03b25bfd85f
Jose Vega <jvega@tulotero.com>
2024-02-11 23:23:48 +0100

    auto updated

---
+++ os/linux/packages/xBuntu/tauri-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo apt update
+sudo apt install -y libwebkit2gtk-4.0-dev \
+	build-essential \
+	curl \
+	wget \
+	file \
+	libssl-dev \
+	libgtk-3-dev \
+	libayatana-appindicator3-dev \
+	librsvg2-dev
+
+echo "Tauri setup complete"
+
+exit 0
