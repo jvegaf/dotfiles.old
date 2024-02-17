32f862f3dbd043f7e506b7e80468145d132fccaf
Jose Vega <josevega234@gmail.com>
2024-02-17 11:36:51 +0100

    disabled fd install

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -8,7 +8,7 @@ install_common_packages() {
 
 	echo "Lazygit installed"
   sh "$DOTFILES_PATH/os/linux/packages/xBuntu/github_cli.sh"
-  sh "$DOTFILES_PATH/os/linux/packages/xBuntu/fd.sh"
+#   sh "$DOTFILES_PATH/os/linux/packages/xBuntu/fd.sh"
   sh "$DOTFILES_PATH/os/linux/packages/xBuntu/sdkman-setup.sh"
   sh "$DOTFILES_PATH/os/linux/packages/xBuntu/neovim-stable.sh"
 }
