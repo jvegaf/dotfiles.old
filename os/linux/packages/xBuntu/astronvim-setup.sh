663f913454b776c731f069f4447155d6e16dcc64
Jose Vega <josevega234@gmail.com>
2024-03-12 14:37:54 +0100

    scripts fixes

--- os/linux/packages/xBuntu/astronvim-setup.sh
+++ os/linux/packages/xBuntu/astronvim-setup.sh
@@ -9,7 +9,7 @@ if [ -d "$NVIM_DIR" ]; then rm -Rf $NVIM_DIR; fi
 
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim $NVIM_DIR
 git clone https://github.com/jvegaf/astronvim_user $NVIM_USER
-cd $NVIM_USER && nvim
+cd $NVIM_USER && nvim --headless +q
 
 exit 0
 
--- os/linux/packages/xBuntu/mint21.sh
+++ os/linux/packages/xBuntu/mint21.sh
@@ -7,5 +7,5 @@ sudo apt-get -y install snapd
 export PATH="$PATH:/snap/bin"
 
 #sudo snap install code --classic
-# sudo snap install intellij-idea-ultimate --classic
-# sudo snap install gitkraken --classic
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install gitkraken --classic
