f1d7b4dfb9374041360addfa7fcf4fc80530d2a1
Jose Vega <josevega234@gmail.com>
2024-03-13 00:45:43 +0100

    kicad 8 script

--- os/linux/packages/xBuntu/kicad-setup.sh
+++ os/linux/packages/xBuntu/kicad-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository --yes ppa:kicad/kicad-7.0-releases
+sudo add-apt-repository --yes ppa:kicad/kicad-8.0-releases
 sudo apt update
 sudo apt install --install-recommends  kicad -y
