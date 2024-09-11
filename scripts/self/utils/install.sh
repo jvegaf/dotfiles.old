46587a45736a2d70ee61567299c534997d2502ce
Jose Vega <josevega234@gmail.com>
2024-09-11 19:54:40 +0200

    nvm version update

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -46,11 +46,11 @@ install_linux_extra() {
 	sudo mv emoji-menu /usr/local/bin
     
 	sh "$DOTFILES_PATH/os/linux/packages/xBuntu/sdkman-setup.sh"
-    sh "$DOTFILES_PATH/os/linux/packages/xBuntu/neovim-stable.sh"
+    #sh "$DOTFILES_PATH/os/linux/packages/xBuntu/neovim-stable.sh"
 }
 
 install_nvm() {
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 	# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 	# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
