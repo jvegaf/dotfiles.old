7c8d82c91cbafd564df321ed0231c5a9aaf5d601
Jose Vega <josevega234@gmail.com>
2025-09-20 06:10:50 +0200

    fix: yazi install script

--- os/linux/packages/xBuntu/yazi.sh
+++ os/linux/packages/xBuntu/yazi.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail
 
-source "$DOTLY_PATH/scripts/core/_main.sh"
-
 sudo apt install -y ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick
 cargo install --locked yazi-fm yazi-cli
-log::note "Yazi FM installed ✅"
 
 exit 0
